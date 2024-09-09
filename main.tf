ff28d5043aebce9c3e2f8cb0238a8167ccbe2c61
shanye <shanye.xzq@alibaba-inc.com>
2024-09-09 17:27:20 +0800

    module: add attributes for alicloud_amqp_queue; examples/complete: improve the examples

--- main.tf
+++ main.tf
@@ -1,14 +1,18 @@
 resource "alicloud_amqp_instance" "default" {
-  count          = var.create ? 1 : 0
-  instance_name  = var.instance_name != "" ? var.instance_name : var.name
-  instance_type  = var.instance_type
-  max_tps        = var.max_tps
-  queue_capacity = var.queue_capacity
-  support_eip    = var.support_eip
-  max_eip_tps    = var.max_eip_tps
-  payment_type   = var.payment_type
-  period         = var.period
-  modify_type    = var.modify_type
+  count                 = var.create ? 1 : 0
+  instance_name         = var.instance_name != "" ? var.instance_name : var.name
+  instance_type         = var.instance_type
+  max_tps               = var.max_tps
+  queue_capacity        = var.queue_capacity
+  support_eip           = var.support_eip
+  max_eip_tps           = var.max_eip_tps
+  payment_type          = var.payment_type
+  period                = var.period
+  renewal_duration      = var.renewal_duration
+  renewal_duration_unit = var.renewal_duration_unit
+  renewal_status        = var.renewal_status
+  modify_type           = var.modify_type
+  storage_size          = var.storage_size
 }
 
 resource "alicloud_amqp_virtual_host" "default" {
@@ -20,6 +24,14 @@ resource "alicloud_amqp_queue" "default" {
   instance_id       = local.amqp_instance_id
   queue_name        = var.queue_name
   virtual_host_name = alicloud_amqp_virtual_host.default.virtual_host_name
+  auto_delete_state = var.auto_delete_state_queue
+  auto_expire_state = var.auto_expire_state
+  dead_letter_exchange = var.dead_letter_exchange
+  dead_letter_routing_key = var.dead_letter_routing_key
+  exclusive_state = var.exclusive_state
+  max_length = var.max_length
+  maximum_priority = var.maximum_priority
+  message_ttl = var.message_ttl
 }
 
 resource "alicloud_amqp_exchange" "default" {
@@ -29,6 +41,7 @@ resource "alicloud_amqp_exchange" "default" {
   instance_id       = alicloud_amqp_virtual_host.default.instance_id
   internal          = var.internal
   virtual_host_name = alicloud_amqp_virtual_host.default.virtual_host_name
+  alternate_exchange = var.alternate_exchange
 }
 
 resource "alicloud_amqp_binding" "default" {
